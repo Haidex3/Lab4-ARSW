520a70dc97ab2313165ce86c9b805e449b32df1e
EmilyNorena <emily.norena-c@mail.escuelaing.edu.co>
2025-09-08 19:19:04 -0500

    images introduction

--- Introduccion/src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ Introduccion/src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -3,8 +3,6 @@ package edu.eci.arsw.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
 
 @Component
 public class GrammarChecker {
--- Introduccion/src/main/java/edu/eci/arsw/springdemo/SpellChecker.java
+++ Introduccion/src/main/java/edu/eci/arsw/springdemo/SpellChecker.java
@@ -1,8 +1,5 @@
 package edu.eci.arsw.springdemo;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public interface SpellChecker {
 
 	public String checkSpell(String text);
