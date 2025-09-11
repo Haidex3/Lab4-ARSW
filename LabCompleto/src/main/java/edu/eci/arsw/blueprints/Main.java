0bc282bbe1e0a740bbe300a2647650e33bf4a0f8
EmilyNorena <emily.norena-c@mail.escuelaing.edu.co>
2025-09-11 10:03:32 -0500

    main

---
+++ LabCompleto/src/main/java/edu/eci/arsw/blueprints/Main.java
@@ -0,0 +1,34 @@
+package edu.eci.arsw.blueprints;
+
+import edu.eci.arsw.blueprints.config.AppConfig;
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        BlueprintsServices services = ctx.getBean(BlueprintsServices.class);
+
+        try {
+            Blueprint bp = new Blueprint("Emily", "Casa", new Point[]{
+                    new Point(10, 10), new Point(20, 20), new Point(30, 30)
+            });
+
+            services.addNewBlueprint(bp);
+
+            Blueprint loaded = services.getBlueprint("Emily", "Casa");
+            System.out.println("Recovered blueprint: " + loaded);
+
+            System.out.println("All blueprints: " + services.getAllBlueprints());
+
+            System.out.println("Emily's Blueprints: " + services.getBlueprintsByAuthor("Emily"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
